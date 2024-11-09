3289c55dddbdff3e912f7cbd4f2b1ecc72ad4761
TARUNORI <147972510+NanogramNo2@users.noreply.github.com>
2024-11-10 08:47:13 +0900

    a

--- main.go
+++ main.go
@@ -11,4 +11,44 @@ import (
 func main(){
 	fmt.PrintIn("Ntool ALPHA 1.0")
 	fmt.PrintIn("CONNECTING SERVER...")
+	ws, dialErr := websocket.Dial("ws://localhost:8000", "", "http://localhost:8000")
+	if dialErr != nil {
+		log.Fatal(dialErr)
+	}
+	defer ws.Close()
+	authagain:
+	int error = 0
+	fmt.Print("TYPE PASSKEY:")
+	fmt.Scan(&key)
+	sendRestMsg(ws, key)
+	fmt.PrintIn(" ")
+	var recvMsg string
+	for {
+		recvErr := websocket.Message.Receive(ws, &recvMsg)
+		if recvErr != nil {
+			log.Fatal(recvErr)
+			break
+		}
+		log.Println("Receive : " + recvMsg)
+	}
+	if recvMsg == "authorized" {
+		
+	}
+	else if recvMsg == "failed" {
+		fmt.PrintIn("Auth : FAILED , TYPE PASSKEY AGAIN")
+		error = error + 1
+		if	error >= 3 {
+			fmt.PrintIn("Error : TOO MANY REQUEST , EXITING SYSTEM")
+		}
+		else{
+			goto authagain
+		}
+	}
+}
+
+func sendRestMsg(ws *websocket.Conn, msg string) {
+	sendErr := websocket.Message.Send(ws, msg)
+	if sendErr != nil {
+		log.Fatal(sendErr)
+	}
 }

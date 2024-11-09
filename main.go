c6d75c0a10567788be9d71b4be76e68d44ee226c
TARUNORI <147972510+NanogramNo2@users.noreply.github.com>
2024-11-09 20:54:42 +0900

    aa

--- main.go
+++ main.go
@@ -2,18 +2,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
 	"syscall"
-	"github.com/nsf/termbox-go"
 )
 
 func main(){
-	if err := termbox.Init(); err != nil {
-        panic(err)
-    }
-    w, h := termbox.Size()
-    termbox.Close()
-	int HalfWidth = (w - 19)/2
-	fmt.PrintIn(HalfWidth,"Ntool VERSION ALPHA",HalfWidth)
-	os.Exit(0)
+	fmt.PrintIn("Ntool ALPHA 1.0")
 }
